faacbcfadfc31a2f067493c441ab930e8de21e82
harsal-patel <harsalanilpatel@gmail.com>
2026-07-23 22:12:35 -0700

    skateboard movement rework to feel more realistic

--- GMTK26/Assets/Player/SkateboardController.cs
+++ GMTK26/Assets/Player/SkateboardController.cs
@@ -5,19 +5,26 @@ using UnityEngine.InputSystem;
 
 public class SkateboardController : MonoBehaviour
 {
+    [Header("Speed")]
+    public float acceleration = 20f;
+    public float maxSpeed = 8f;
+
+    [Header("Turning")]
+    public float turnSpeed = 180f;
+    [Range(0f, 1f)]
+    public float minTurnSpeedFactor = 0.25f;
+
+    [Header("Feel")]
+    public float grip = 8f;
 
     Rigidbody rb;
-    public float forwardForce = 20f;
-    public float torque = 4f;
-
-    [SerializeField]
-    private PlayerInput playerInput;
-    private InputAction movementAction;
+    PlayerInput playerInput;
+    InputAction movementAction;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        rb.maxLinearVelocity = 20;
+        rb.maxLinearVelocity = maxSpeed;
 
         InputSetup();
     }
@@ -31,21 +38,24 @@ public class SkateboardController : MonoBehaviour
 
     void FixedUpdate()
     {
-        if(movementAction.ReadValue<Vector2>().y > 0)
-        {
-            rb.AddForce(forwardForce * transform.forward);
-        }
-        if(movementAction.ReadValue<Vector2>().y < 0)
-        {
-            rb.AddForce(forwardForce * -transform.forward);
-        }
-        if(movementAction.ReadValue<Vector2>().x > 0)
-        {
-            rb.AddTorque(Vector3.up * torque * Time.fixedDeltaTime);
-        }
-        if(movementAction.ReadValue<Vector2>().x < 0)
-        {
-            rb.AddTorque(Vector3.up * -torque * Time.fixedDeltaTime);
-        }
+        // For live tuning
+        rb.maxLinearVelocity = maxSpeed;
+
+        Vector2 input = movementAction.ReadValue<Vector2>();
+
+        rb.AddForce(transform.forward * input.y * acceleration, ForceMode.Acceleration);
+
+        float speedFactor = Mathf.Clamp01(rb.velocity.magnitude / maxSpeed);
+
+        // Turn faster while moving, slower while not
+        float turnFactor = Mathf.Lerp(minTurnSpeedFactor, 1f, speedFactor);
+        float turnThisFrame = input.x * turnSpeed * turnFactor * Time.fixedDeltaTime;
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(0f, turnThisFrame, 0f));
+
+        // Smoother velocity realignment
+        Vector3 flatVelocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        Vector3 targetVelocity = transform.forward * flatVelocity.magnitude;
+        Vector3 carvedVelocity = Vector3.Lerp(flatVelocity, targetVelocity, grip * Time.fixedDeltaTime);
+        rb.velocity = new Vector3(carvedVelocity.x, rb.velocity.y, carvedVelocity.z);
     }
 }

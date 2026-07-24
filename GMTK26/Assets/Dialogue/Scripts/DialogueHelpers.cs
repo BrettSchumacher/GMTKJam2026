80da83adaca8d50d27aa4d8193e64c6bcc8e8b5d
BrettSchumacher <bwillshoe@gmail.com>
2026-07-23 21:56:15 -0700

    Update dialogue helper to new csv format

--- GMTK26/Assets/Dialogue/Scripts/DialogueHelpers.cs
+++ GMTK26/Assets/Dialogue/Scripts/DialogueHelpers.cs
@@ -1,25 +1,27 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using UnityEngine;
 
 public struct ConversationEntry
 {
     public bool IsPlayer;
+    public string CharacterName;
     public string[] Entries;
 
-    // Should only have multiple entries for player dialogue options
-    public ConversationEntry(string[] entries, bool isPlayer = true)
+    // Constructor with multiple entries is only for Player
+    public ConversationEntry(string[] entries, string name)
     {
         Entries = entries;
-        IsPlayer = isPlayer;
+        IsPlayer = true;
+        CharacterName = name;
     }
 
-    public ConversationEntry(string entry, bool isPlayer)
+    // Constructor with one entry is meant for Npcs
+    public ConversationEntry(string entry, string name, bool isPlayer = false)
     {
         Entries = new string[1] { entry };
+        CharacterName = name;
         IsPlayer = isPlayer;
     }
 
@@ -27,15 +29,26 @@ public struct ConversationEntry
     {
         return Entries.Length > 0;
     }
+
+    public bool IsPlayerChoice()
+    {
+        return IsPlayer && Entries.Length > 1;
+    }
 }
 
 public class ConversationData
 {
     public List<ConversationEntry> ConversationEntries = new();
+    public string[] Characters { get; protected set; } // All the characters present in this conversation
 
     public void AddEntry(ConversationEntry entry)
     {
         ConversationEntries.Add(entry);
+    }
+
+    public void SetCharacters(string[] characters)
+    {
+        Characters = characters;
     }
 }
 
@@ -46,9 +59,25 @@ public static class DialogueHelpers
         ConversationData conversation = new();
         string[] csvRows = csvString.Split('\n');
 
-        foreach (string row in csvRows)
+        if (csvRows.Length < 2)
         {
-            ConversationEntry entry = LoadEntryFromCsvRow(row);
+            Debug.LogError("DialogueHelpers::LoadConversationFromCsvString - Conversations need at least 2 rows, character row and dialogue");
+            return new ConversationData();
+        }
+
+        string[] characters = LoadCharactersCsvRow(csvRows[0]);
+        if (characters.Length < 1)
+        {
+            Debug.LogError("DialogueHelpers::LoadConversationFromCsvString - Conversations need at least 1 character");
+            return new ConversationData();
+        }
+        
+        conversation.SetCharacters(characters);
+
+        for (int i = 1; i < csvRows.Length; ++i)
+        {
+            string row = csvRows[i];
+            ConversationEntry entry = LoadEntryFromCsvRow(row, characters);
             if (entry.IsValid())
             {
                 conversation.AddEntry(entry);
@@ -58,7 +87,90 @@ public static class DialogueHelpers
         return conversation;
     }
 
-    private static ConversationEntry LoadEntryFromCsvRow(string csvRow)
+    // Load the first row of csv entries as the characters present in the conversation.
+    // The last entry is assumed to be the player
+    private static string[] LoadCharactersCsvRow(string csvRow)
+    {
+        List<string> csvFields = SplitCsvRow(csvRow);
+        List<string> characters = new();
+
+        bool charactersEnded = false;
+        foreach (string field in csvFields)
+        {
+            if (field.Length == 0)
+            {
+                charactersEnded = true;
+                continue;
+            }
+            
+            // if not empty but previous entry was empty then error
+            if (charactersEnded)
+            {
+                Debug.LogError("DialogueHelpers:LoadCharactersCsvRow - Cannot have middle character name entry be empty");
+                return Array.Empty<string>();
+            }
+            
+            characters.Add(field);
+        }
+
+        return characters.ToArray();
+    }
+
+    // Load conversation entry from row given characters present
+    private static ConversationEntry LoadEntryFromCsvRow(string csvRow, string[] characters)
+    {
+        List<string> csvFields = SplitCsvRow(csvRow);
+
+        if (csvFields.Count < characters.Length)
+        {
+            Debug.LogError("DialogueHelpers::LoadEntryFromCsvRow - conversation row has fewer entries characters: " + csvRow);
+            return new ConversationEntry("", "error");
+        }
+
+        // To see if it's player dialogue go through and test if there's any fields filled before the final character (player) column
+        bool isPlayer = true;
+        int characterIndex = characters.Length - 1;
+        for (int i = 0; i < characters.Length - 2; ++i)
+        {
+            if (csvFields[i].Length > 0)
+            {
+                // If we already found dialogue under another character then this is saying there's text from multiple characters
+                if (!isPlayer)
+                {
+                    Debug.LogError("DialogueHelpers::LoadEntryFromCsvRow - conversation row has multiple non-player entries: " + csvRow);
+                    return new ConversationEntry("", "error");
+                }
+                isPlayer = false;
+                characterIndex = i;
+            }
+        }
+
+        // if not player, take the one line of dialogue we care about and use it
+        if (!isPlayer)
+        {
+            string dialogueLine = csvFields[characterIndex];
+            csvFields.RemoveAt(characterIndex);
+            if (csvFields.Any(field => field.Length > 0))
+            {
+                Debug.LogError("DialogueHelpers::LoadEntryFromCsvRow - Invalid row has both npc and player columns filled: " + csvRow);
+                return new ConversationEntry("", "error");
+            }
+
+            return new ConversationEntry(dialogueLine, characters[characterIndex]);
+        }
+
+        // if it is the player, remove all empty columns and just collect the dialogue lines we care about
+        csvFields.RemoveAll(field => field.Length == 0);
+        if (csvFields.Count == 0)
+        {
+            Debug.LogWarning("DialogueHelpers::LoadEntryFromCsvRow - Conversation contained fully empty row " + csvRow);
+            return new ConversationEntry("", "error");
+        }
+
+        return new ConversationEntry(csvFields.ToArray(), characters.Last());
+    }
+
+    private static List<string> SplitCsvRow(string csvRow)
     {
         List<string> csvFields = new();
         bool insideQuote = false;
@@ -98,27 +210,6 @@ public static class DialogueHelpers
 
         csvFields.Add(currentField);
 
-        if (csvFields[0].Length > 0)
-        {
-            // First entry populated means it's npc line
-            string npcLine = csvFields[0];
-            csvFields.RemoveAt(0);
-            if (csvFields.Any())
-            {
-                Debug.LogError("DialogueHelpers::LoadEntryFromCsvRow - Invalid row has entries in first column and following column(s): " + csvRow);
-                return new ConversationEntry("", false);
-            }
-
-            return new ConversationEntry(npcLine, false);
-        }
-
-        csvFields.RemoveAll(field => field.Length == 0);
-        if (csvFields.Count == 0)
-        {
-            Debug.LogWarning("DialogueHelpers::LoadEntryFromCsvRow - Conversation contained fully empty row " + csvRow);
-            return new ConversationEntry("", false);
-        }
-
-        return new ConversationEntry(csvFields.ToArray());
+        return csvFields;
     }
 }

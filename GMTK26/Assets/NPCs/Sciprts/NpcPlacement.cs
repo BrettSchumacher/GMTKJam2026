746bd1a80a6e86564ff4716ef263bd4636395ea4
BrettSchumacher <bwillshoe@gmail.com>
2026-07-25 12:48:31 -0700

    Remove redundant placement info

--- GMTK26/Assets/NPCs/Sciprts/NpcPlacement.cs
+++ GMTK26/Assets/NPCs/Sciprts/NpcPlacement.cs
@@ -3,20 +3,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum NpcPlacementType
-{
-    INVALID,
-    INITIAL, // Placement player encounters them first in main game area
-    FINAL // Placement for final skatepark scene.
-}
-
 public class NpcPlacement : MonoBehaviour
 {
     public static Dictionary<string, List<NpcPlacement>> NpcPlacementsById;
     
     public string NpcId;
     public int ConversationIndex = 0;
-    public NpcPlacementType PlacementType;
 
     private void Awake()
     {

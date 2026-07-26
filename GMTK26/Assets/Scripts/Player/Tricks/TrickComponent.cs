3aab9aa7e7fd92916f8d576d192dba8d7ff262d0
J. Phadnis <jasonphadnis@gmail.com>
2026-07-26 01:00:49 -0500

    Added 4 animations for tricks.

--- GMTK26/Assets/Scripts/Player/Tricks/TrickComponent.cs
+++ GMTK26/Assets/Scripts/Player/Tricks/TrickComponent.cs
@@ -74,9 +74,16 @@ public class TrickComponent : MonoBehaviour
     private InputAction flipAction;
     private InputAction grindAction;
 
+    private Animator skateboardAnimator;
+
     private void Awake()
     {
         LoadTricks();
+    }
+
+    private void Start()
+    {
+        skateboardAnimator = GetComponent<Animator>();
     }
 
     private void OnEnable()
@@ -129,6 +136,8 @@ public class TrickComponent : MonoBehaviour
     public void ExecuteTrick(TrickMatch TrickToDo)
     {
         Debug.Log($"Did a {TrickToDo.Trick.GetDisplayName(TrickToDo.TapCount)} (AnimID: {TrickToDo.Trick.GetAnimationID(TrickToDo.TapCount)}, Points: {TrickToDo.Trick.GetPointValue(TrickToDo.TapCount)}");
+
+        skateboardAnimator.Play(TrickToDo.Trick.AnimationID, 0, 0f);
     }
 
 
